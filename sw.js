1153e0d5e1112ac0e282b7481e1b85889d7c648b
conexaosmarthome <conexaosmarthome@gmail.com>
2026-04-10 17:12:03 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,29 @@
-const CACHE = 'led-calc-v1';
+const CACHE_NAME = 'conexao-led-v3';
+const FILES = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./index.html'])));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
 });
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
 });
